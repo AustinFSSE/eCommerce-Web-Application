c25e240407e399d16a637a2eaa49a8432df60841
AustinFSSE <108036029+AustinFSSE@users.noreply.github.com>
2025-04-29 14:10:04 -0400

    fixed tailwind error. Adding tailwind to vite config dependency list

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import {FaBeer} from "react-icons/fa";
+import Products from "./components/Products.jsx";
+import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className={"flex items-center justify-center h-screen bg-grey-800 text-white text-2xl font-bold"}>
-        <p>Welcome to react <FaBeer/></p>
+        <Products/>
     </div>
   )
 }
